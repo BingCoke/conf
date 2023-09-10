ff34124c2f3e5a7de8f10e74172477f4202f8c9d
bingcoke <bingcoke@qq.com>
2023-09-10 15:15:57 +0800

    dilay

--- autostart.sh
+++ autostart.sh
@@ -3,40 +3,31 @@ source ~/.profile
 
 # 设置状态烂
 $DWM/statusbar/statusbar.sh cron &
-# 定时更新订阅
-#
-#
-# changeBg() {
-#   while true
-#   do
-#     sleep 1000
-#     feh --bg-fill --randomize ~/tools/wallpaper/*.png
-#   done
-# }
-#
-# changeBg &
-#glava -d &> /dev/null &
 
 settings() {
     xset -b                                   # 关闭蜂鸣器
     syndaemon -i 1 -t -K -R -d                # 设置使用键盘时触控板短暂失效
-    ~/tools/scripts/set_screen.sh two             # 设置显示器
+    #~/mygithub/conf/scripts/set_screen.sh two             # 设置显示器
+    xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output DP-4 --off --output eDP-1-1 --mode 1680x1050 --pos 0x0 --rotate normal
+
+
+    sudo mount /dev/sda1 /home/bk/data &
 }
 
 daemons() {
   xfce4-power-manager &
-  # picom --config /home/bk/.config/picom-a.conf  --experimental-backends -b
-  picom -b
+  picom --config /home/bk/.config/picom.conf -b
+  #picom -b
   fcitx5 -d
   copyq &
-#  clash &
   clash-verge &
+  feh --bg-fill --randomize ~/mygithub/conf/wallpaper/*.png
 }
 
 cron() {
     sleep 5s
     while true; do
-        ~/tools/scripts/set_screen.sh check               # 设置显示器
+        #~/mygithub/conf/scripts/set_screen.sh check               # 设置显示器
         sleep 5; 
     done
 }
